aa9e757305b182c4afada82a5a42a6e1a1f28144
fancy88 <1205090178@qq.com>
2017-01-12 17:58:49 +0800

    初始化版本

---
+++ WonlyIntelligenceNew/WonlyIntelligenceNew/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  WonlyIntelligenceNew
+//
+//  Created by apple on 17/1/12.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
