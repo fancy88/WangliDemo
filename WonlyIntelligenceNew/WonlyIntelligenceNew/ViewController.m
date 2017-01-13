6c1f4b19b1325aa37e3566795edf88de7bd67366
fancy88 <1205090178@qq.com>
2017-01-13 10:28:41 +0800

    修改主页面

--- WonlyIntelligenceNew/WonlyIntelligenceNew/ViewController.m
+++ WonlyIntelligenceNew/WonlyIntelligenceNew/ViewController.m
@@ -16,8 +16,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+   
     
+    [self getUI];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)getUI{
+    
+    UITextField *nameTF = [[UITextField alloc] initWithFrame:CGRectMake(SCREENWIDTH / 2.0 - 100, 200, 200, 30)];
+    nameTF.placeholder = @"请输入您的账号";
+    UILabel *nameL = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 30)];
+    nameL.text = @"账号:";
+    nameTF.leftView = nameL;
+    nameTF.leftViewMode = UITextFieldViewModeAlways;
+    [self.view addSubview:nameTF];
+    
+    UITextField *passTF = [[UITextField alloc] initWithFrame:CGRectMake(SCREENWIDTH / 2.0 - 100, 250, 200, 30)];
+    passTF.placeholder = @"请输入您的密码";
+    UILabel *passL = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 30)];
+    passL.text = @"密码:";
+    passTF.leftView = passL;
+    passTF.leftViewMode = UITextFieldViewModeAlways;
+    [self.view addSubview:passTF];
+    
 }
 
 
